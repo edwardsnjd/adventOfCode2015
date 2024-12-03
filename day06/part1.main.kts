dcd6b4ec54af3f3ece79c87f393d729995ae58b1
Nicholas Edwards <edwardsnjd@googlemail.com>
2024-12-03 08:15:20 +0800

    wip: Clean up

--- day06/part1.main.kts
+++ day06/part1.main.kts
@@ -13,10 +13,6 @@ val commandRe = Regex(
   """^(?<op>turn on|turn off|toggle) (?<x1>\d+),(?<y1>\d+) through (?<x2>\d+),(?<y2>\d+)$"""
 )
 
-fun turnOn(_: Boolean) = true
-fun turnOff(_: Boolean) = false
-fun toggle(v: Boolean) = !v
-
 fun intoCommand(line: String) -> Command {
   val match = commandRe.match(line)!!
   return Command(
@@ -25,31 +21,38 @@ fun intoCommand(line: String) -> Command {
     y1 = match["y1"],
     x2 = match["x2"],
     y2 = match["y2"],
-    operation = when match["op"] {
-      "turn on" -> ::turnOn,
-      "turn off" -> ::turnOff,
-      _ -> ::toggle,
-    },
   )
+}
+
+fun turnOn(_: Boolean) = true
+fun turnOff(_: Boolean) = false
+fun toggle(v: Boolean) = !v
+fun operation(op: String) = when op {
+  "turn on" -> ::turnOn,
+  "turn off" -> ::turnOff,
+  _ -> ::toggle,
 }
 
 // ENTRY POINT
 
+val commands = generateSequence(::readln).map(::intoCommand)
+
 val grid = BooleanArray(1000000)
 
-generateSequence(::readln)
-  .map(::intoCommand)
-  .forEach {
-    println(it)
-    val xMin = Math::min(it.x1, it.x2)
-    val xMax = Math::max(it.x1, it.x2)
-    val yMin = Math::min(it.y1, it.y2)
-    val yMax = Math::max(it.y1, it.y2)
-    for y in yMin..yMax {
-      for x in xMin..xMax {
-        val idx = y * 1000 + x
-        grid[idx] = it.operation(grid[idx])
-      }
+commands.forEach {
+  println(it)
+  val xMin = Math::min(it.x1, it.x2)
+  val xMax = Math::max(it.x1, it.x2)
+  val yMin = Math::min(it.y1, it.y2)
+  val yMax = Math::max(it.y1, it.y2)
+  val operation = command(it.op)
+
+  for y in yMin..yMax {
+    for x in xMin..xMax {
+      val idx = y * 1000 + x
+      grid[idx] = operation(grid[idx])
     }
+  }
+}
 
 grid.count { it }
